f153fd4e537cf075e994950b0ceab030926b7317
Toastek <giacomo.kersevan@epitech.eu>
2018-12-10 16:48:43 +0100

    added cloud_firestore to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:partnership/ui/login_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(new MyApp());
 
