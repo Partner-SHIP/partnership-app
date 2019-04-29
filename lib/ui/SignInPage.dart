871c9fef7ca53cef798238d57edad3f8dd8f28c5
victor <victor.gsell@epitech.eu>
2019-04-29 18:31:18 +0200

    Customized text inputs, Added provider's icons

--- lib/ui/SignInPage.dart
+++ lib/ui/SignInPage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:partnership/utils/Routes.dart';
 import 'package:partnership/viewmodel/AViewModelFactory.dart';
 import 'package:partnership/viewmodel/SignInPageViewModel.dart';
-import 'package:partnership/style/theme.dart';
 import 'package:partnership/ui/widgets/ThemeContainer.dart';
+import 'package:partnership/ui/widgets/RoundedGradientButton.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'dart:async';
 import 'dart:ui';
 
@@ -183,11 +183,12 @@ class _SignInPageState extends State<SignInPage> {
           Expanded(
             child: FlatButton(
                 onPressed: () => setState(() => _switch = !_switch),
-                child: Text(
-                  'Email et Mdp',
+                child: AutoSizeText(
+                  'Email',
+                  maxLines: 1,
                   softWrap: false,
                   style: TextStyle(
-                      color: _switch ? null : Colors.white,
+                      color: _switch ? Colors.grey : Colors.white,
                       fontFamily: 'Orkney',
                       fontSize: 15
                   ),
@@ -196,7 +197,7 @@ class _SignInPageState extends State<SignInPage> {
           ),
           ClipRect(
               child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 0.0, sigmaY: 0.0),
+                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
                 child: Container(
                   width: 2,
                   height: MediaQuery.of(context).size.height / 8,
@@ -209,17 +210,153 @@ class _SignInPageState extends State<SignInPage> {
           Expanded(
             child: FlatButton(
               onPressed: () => setState(() => _switch = !_switch),
-              child: Text(
+              child: AutoSizeText(
                 'Réseaux Sociaux',
+                maxLines: 1,
                 softWrap: false,
                 style: TextStyle(
-                    color: _switch ? Colors.white : null,
+                    color: _switch ? Colors.white : Colors.grey,
                     fontFamily: 'Orkney',
                     fontSize: 15
                 ),
               ),
             ),
           ),
+        ],
+      ),
+    );
+
+    final formContainer = Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height / 2,
+      //color: Colors.blue,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                maxLines: 1,
+                maxLength: 30,
+                onSaved: null,
+                decoration: InputDecoration(
+                  hintText: 'Addresse email valide',
+                  labelStyle: TextStyle(fontFamily: "Orkney"),
+                  hintStyle: TextStyle(fontFamily: "Orkney"),
+                  icon: Padding(
+                    child: Icon(Icons.mail, size: 30, color: Colors.white),
+                    padding: EdgeInsets.only(top: 25)
+                  )
+                )
+              ),
+              padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width / 5,
+                right: MediaQuery.of(context).size.width / 5
+              )
+            ),
+            Padding(
+              child: TextFormField(
+                maxLines: 1,
+                maxLength: 30,
+                obscureText: true,
+                onSaved: null,
+                decoration: InputDecoration(
+                  hintText: 'Mot de passe',
+                  icon: Padding(
+                    child: Icon(Icons.vpn_key, size: 30, color: Colors.white),
+                    padding: EdgeInsets.only(top: 25)
+                  )
+                ),
+              ),
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width / 5,
+                    right: MediaQuery.of(context).size.width / 5
+                )
+            ),
+            Padding(
+              child: RoundedGradientButton(
+                child: AutoSizeText(
+                  'CONNEXION',
+                  maxLines: 1,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Orkney'
+                  ),
+                ),
+                callback: null,
+                increaseWidthBy: 80,
+                increaseHeightBy: 10
+              ),
+              padding: EdgeInsets.only(top: 40),
+            )
+          ],
+        ),
+      ),
+    );
+
+    final providersContainer = Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height / 2,
+//      color: Colors.blue,
+      child: Stack(
+        children: <Widget>[
+          Center(child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Container(
+                  width: 2,
+                  height: MediaQuery.of(context).size.height / 3,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.3),
+                  ),
+                ),
+              )
+          )),
+          Center(child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.height / 3,
+                  height: 2,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.3),
+                  ),
+                ),
+              )
+          )),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  MaterialButton(
+                    child: Image.asset('assets/img/login_provider_google.png', width: 60, height: 60),
+                    onPressed: null,
+                  ),
+                  MaterialButton(
+                    child: Image.asset('assets/img/login_provider_fb.png', width: 60, height: 60),
+                    onPressed: null,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  MaterialButton(
+                    child: Image.asset('assets/img/login_provider_insta.png', width: 60, height: 60),
+                    onPressed: null,
+                  ),
+                  MaterialButton(
+                    child: Image.asset('assets/img/login_provider_twitter.png', width: 60, height: 60),
+                    onPressed: null,
+                  )
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
@@ -234,15 +371,20 @@ class _SignInPageState extends State<SignInPage> {
                   child: Image.asset('assets/img/partnership_logo.png', width:110, height: 110),
                   padding: EdgeInsets.only(top: 15, bottom: 15),
                 ),
-                Text(
+                AutoSizeText(
                   'Connexion',
+                  maxLines: 1,
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 35,
                     fontFamily: 'Orkney'
                   ),
                 ),
-                loginSwitcher
+                loginSwitcher,
+                Padding(
+                  child: _switch ? providersContainer : formContainer,
+                  padding: EdgeInsets.only(top: 10),
+                )
               ],
             ))
         )
