532e278f7f43e1775ffa835df9a9cd7b296c4e00
hoarauv <hoarau.victor@gmail.com>
2019-05-07 19:27:42 +0200

    init model

--- lib/model/ProjectBrowsingPageModel.dart
+++ lib/model/ProjectBrowsingPageModel.dart
@@ -1,5 +1,20 @@
 import 'package:partnership/model/AModel.dart';
 import 'package:partnership/utils/FBCollections.dart';
+import 'package:partnership/model/ATableModel.dart';
+
+class ProjectSlideData {
+
+}
+
+class ProjectSlideListModel extends ATableModel {
+  ProjectSlideListModel();
+
+  @override
+  void readJSON() {
+    
+  }
+  
+}
 
 class ProjectBrowsingPageModel extends AModel {
   ProjectBrowsingPageModel(): super();
