80dc4a6d23a0f97a6921180968a433226ff016da
Toastek <giacomo.kersevan@epitech.eu>
2019-11-29 21:44:45 +0100

    started editmembers / toastek

--- lib/ui/ProjectManagementTabs/EditMembersTab.dart
+++ lib/ui/ProjectManagementTabs/EditMembersTab.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:partnership/ui/widgets/ThemeContainer.dart';
 import 'package:partnership/viewmodel/ProjectManagementPageViewModel.dart';
 import 'package:partnership/viewmodel/ProjectManagementTabsViewModel.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
 class EditMembersTab extends StatelessWidget {
   final ProjectManagementTabsViewModel viewModel;
-  final Project                        project;
-  EditMembersTab(ProjectManagementTabsViewModel vm, Project project) : viewModel = vm, project = project;
+  final Project project;
+  EditMembersTab(ProjectManagementTabsViewModel vm, Project project)
+      : viewModel = vm,
+        project = project;
   @override
   Widget build(BuildContext context) {
-    return AutoSizeText("Edit Members Tab");
+    // return AutoSizeText("Edit Members Tab");
+    return ListView(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Text('data'),
+            Text('toto'),
+          ],
+        ),
+        ListTile(
+          title: Text('Moon'),
+        ),
+        ListTile(
+          title: Text('Star'),
+        ),
+      ],
+    );
   }
 }
