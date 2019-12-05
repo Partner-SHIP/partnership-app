f8680d378333ce04ec0c47215e378dc3e55560ec
Quentin Capra <quentin.capra@epitech.eu>
2019-12-05 17:32:03 +0100

    fix commentary bug

--- lib/ui/widgets/CommentaryList.dart
+++ lib/ui/widgets/CommentaryList.dart
@@ -2,39 +2,40 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 
-Container commentaryList(context){
+Container commentaryList(context) {
   return Container(
-    child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height - 110,
-                    child: StreamBuilder<QuerySnapshot>(
-                        stream: Firestore.instance
-                            .collection('projects')
-                            .snapshots(),
-                        builder: (BuildContext context,
-                            AsyncSnapshot<QuerySnapshot> snapshot) {
-                          if (snapshot.hasError)
-                            return new Text('Error: ${snapshot.error}');
-                          switch (snapshot.connectionState) {
-                            case ConnectionState.waiting:
-                              return new Text('Loading...');
-                            default:
-                              return new ListView(
-                                children: snapshot.data.documents
-                                    .map((DocumentSnapshot document) {
-                                     print(document.data['commentaire'][0]['message']);
-                                      return new ListTile(
-                                        title:  Text(document.data['commentaire'][0]['message'] ?? 'title not found'),
-                                        //subtitle: new Text(document['name']),
-                                      );
-                                    }).toList(),
-                              );
-                          }
-                        }))
-      ]
-    ,)
-  );
-
+      child: Column(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height - 110,
+          child: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection('projects').snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError)
+                  return new Text('Error: ${snapshot.error}');
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return new Text('Loading...');
+                  default:
+                    final int messageCount = snapshot.data.documents.length;
+                    return new ListView.builder(
+                        itemCount: messageCount,
+                        itemBuilder: (_, int index) {
+                          final DocumentSnapshot document =
+                              snapshot.data.documents[index];
+                          print(document['commentaire'][index]['message']);
+                          return new ListTile(
+                            title: Text(document['commentaire'][index]
+                                    ['message'] ??
+                                'title not found'),
+                            subtitle: new Text(document['name']),
+                          );
+                        });
+                }
+              }))
+    ],
+  ));
 }
